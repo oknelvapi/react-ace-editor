c857805ab5cf2055cde2b2d7eb4fab833e38f73b
Ihor P <oknelvapi@yahoo.com>
2019-07-03 10:31:51 +0300

    Updates

---
+++ precache-manifest.4f2201a86d43bc73d609361c47830d72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1484e4e588d828ca2becf2e4dfb83db3",
+    "url": "/react-ace-editor/index.html"
+  },
+  {
+    "revision": "704583e9600b9db8a6c2",
+    "url": "/react-ace-editor/static/css/main.c5b05437.chunk.css"
+  },
+  {
+    "revision": "bdf579ae9fffe1a51b3c",
+    "url": "/react-ace-editor/static/js/2.aa307ca2.chunk.js"
+  },
+  {
+    "revision": "704583e9600b9db8a6c2",
+    "url": "/react-ace-editor/static/js/main.4288efb1.chunk.js"
+  },
+  {
+    "revision": "ff31e48c94c244fd78e4",
+    "url": "/react-ace-editor/static/js/runtime~main.d86f51b8.js"
+  }
+]);
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './reset.css';
-import App from './common/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
